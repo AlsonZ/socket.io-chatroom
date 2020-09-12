efae867d3b945c2e6e7e185af48d0cb2fd8a0109
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-09-12 16:52:43 +1000

    create getUser function

--- utils/users.js
+++ utils/users.js
@@ -1,12 +1,17 @@
 const users = [];
 
 const createUser = (id, username, room) => {
-  const user = {id, username, room}
+  const user = { id, username, room }
   users.push(user);
   return user;
 }
 
+const getUser = (id) => {
+  return users.find(user => user.id === id);
+}
+
 
 module.exports = {
-  createUser
+  createUser,
+  getUser
 }
