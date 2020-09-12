f6ad3d239e82cf9e9196d51fc6cb727f3eb404ca
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-09-12 16:57:10 +1000

    create message formatter

--- utils/messages.js
+++ utils/messages.js
@@ -1,6 +1,12 @@
 
 const messageFormat = (author, message) => {
-
+  var today = new Date();
+  return {
+    author,
+    message,
+    date: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate(),
+    time: today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+  }
 }
 
 
