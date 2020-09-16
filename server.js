ddb763b8fe0bfc5ffc92f40b60b0af529ac61a58
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-09-16 22:01:59 +1000

    implement backend socketio message handling

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const http = require('html');
+const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
-const {createUser} = require('./utils/users');
+const { createUser, getUser } = require('./utils/users');
+const { messageFormat } = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -13,10 +14,26 @@ app.use(express.static(path.join(__dirname, 'client')));
 const serverName = 'Server';
 
 io.on('connection', (socket) => {
+  // user joins a room
   socket.on('joinRoom', ({username, room}) => {
+    console.log(`${username} has joined room ${room}`)
     const user = createUser(socket.id, username, room);
     socket.join(user.room);
 
-    socket.emit('message', )
-  })
+    // welcome the user
+    io.to(user.room).emit('message', messageFormat('Server', `Welcome ${user.username}`));
+  });
+
+  // user sends a message
+  socket.on('chatMessage', (data) => {
+    const user = getUser(socket.id);
+
+    // send message to all users including author
+    io.to(user.room).emit('message', messageFormat(user.username, data.message));
+  }); 
+
+
 });
+
+
+server.listen(process.env.PORT || 3000, () => {console.log(`Server is running on port ${server.address().port}`)})
